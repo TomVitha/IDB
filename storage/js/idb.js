3c66903866aaa31ab514c25b705bc6569c1876a2
Tom Vitha <tomasvitha01@gmail.com>
2025-04-17 16:18:53 +0200

    Never Ending Cooodiiing

--- storage/js/idb.js
+++ storage/js/idb.js
@@ -1,5 +1,8 @@
-$( document ).ready(function() {
-    Fancybox.bind("[data-fancybox]", {
-        // Your custom options
-      });
-});
+// $( document ).ready(function() {
+//     Fancybox.bind("[data-fancybox]", {
+//       caption: function (fancybox, slide) {
+//         const figurecaption = slide.triggerEl?.querySelector("figurecaption");
+//         return figurecaption ? figurecaption.innerHTML : slide.caption || "";
+//       },
+//     });
+// });
